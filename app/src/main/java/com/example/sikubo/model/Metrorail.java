ae48b3ae53b1c35b524377b029bb4fc5b159f0e7
omeyer <meyeroliver9401@gmail.com>
2019-02-25 13:30:40 +0200

    Created a subclass from PublicTransportMotorVehicle to handle info on Metrorail

---
+++ app/src/main/java/com/example/sikubo/model/Metrorail.java
@@ -0,0 +1,8 @@
+package com.example.sikubo.model;
+
+public class Metrorail extends PublicTransportMotorVehicle {
+
+    public Metrorail(String id, String longName, String mode, String color, String agencyName) {
+        super(id, "", longName, mode, color, agencyName);
+    }
+}
