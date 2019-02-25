a9f261102dbf1794954797dbc01b3f35dec2ed12
omeyer <meyeroliver9401@gmail.com>
2019-02-25 13:27:09 +0200

    Set up a subclass of PublicTrasportMotorVehicle to handle the Golden Arrow busses

---
+++ app/src/main/java/com/example/sikubo/model/GoldenArrow.java
@@ -0,0 +1,7 @@
+package com.example.sikubo.model;
+
+public class GoldenArrow extends PublicTransportMotorVehicle {
+    public GoldenArrow(String id, String shortName, String longName, String mode, String color, String agencyName) {
+        super(id, shortName, longName, mode, color, agencyName);
+    }
+}
