409e9a5f8f4caddb9271da33952e796db0d472a0
omeyer <meyeroliver9401@gmail.com>
2019-02-25 13:31:09 +0200

    Created a subclass from PublicTransportMotorVehicle to handle info on Myciti

---
+++ app/src/main/java/com/example/sikubo/model/MyCiti.java
@@ -0,0 +1,8 @@
+package com.example.sikubo.model;
+
+public class MyCiti extends PublicTransportMotorVehicle {
+
+    public MyCiti(String id, String shortName, String longName, String mode, String color, String agencyName) {
+        super(id, shortName, longName, mode, color, agencyName);
+    }
+}
