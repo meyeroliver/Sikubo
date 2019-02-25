efe5e40215cd7ba3581824bc379b93aca1c21989
omeyer <meyeroliver9401@gmail.com>
2019-02-25 13:36:31 +0200

    GoMetroApi interface that uses retrofit handles http requests

---
+++ app/src/main/java/com/example/sikubo/GoMetroApi.java
@@ -0,0 +1,49 @@
+package com.example.sikubo;
+
+import com.example.sikubo.model.GoldenArrow;
+import com.example.sikubo.model.Metrorail;
+import com.example.sikubo.model.MyCiti;
+import com.example.sikubo.model.Route;
+import com.example.sikubo.model.Stop;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GoMetroApi {
+
+    /**
+     * MetroRail routes
+     */
+    @GET("/rail/routes")
+    Call<List<Metrorail>> getAllRailRoutes();
+    @GET("/rail/routes/{id}/stops")
+    Call<List<Route>> getAllStopsOnARailRoute(@Path("id") String id);
+    @GET("rail/stop/{id}")
+    Call<Stop> getRailStopInfo(@Path("id") String id);
+    @GET("rail/lineupdates/{id}")
+    Call<Stop> getRailLineUpdats(@Path("id") String id);
+
+    /**
+     * MyCiti routes
+     */
+    @GET("/myciti/routes/")
+    Call<List<MyCiti>> getAllTramRoutes();
+    @GET("/myciti/routes/{id}/stops")
+    Call<List<Route>> getAllStopsOnATramRoute(@Path("id") String id);
+    @GET("/myciti/stop/{id}")
+    Call<Stop> getTramStopInfo(@Path("id") String id);
+
+    /**
+     * Golden Arrow routes
+     */
+    @GET("/ga/routes")
+    Call<List<GoldenArrow>> getAllBusRoutes();
+    @GET("/ga/routes/{id}/stops")
+    Call<List<Route>> getAllStopsOnABusRoute(@Path("id") String id);
+    @GET("/ga/stop/{id}")
+    Call<List<Stop>> getBusStopInfo(@Path("id") String id);
+
+}
