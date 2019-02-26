91e8b453e550c0ad6ccc3921eb3a3ea2f96c5325
omeyer <meyeroliver9401@gmail.com>
2019-02-26 12:04:29 +0200

    Added liveData obsever on the routes when obtained and updated

--- app/src/main/java/com/example/sikubo/viewmodel/RouteViewModel.java
+++ app/src/main/java/com/example/sikubo/viewmodel/RouteViewModel.java
@@ -1,6 +1,17 @@
 package com.example.sikubo.viewmodel;
 
+import com.example.sikubo.model.Route;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 public class RouteViewModel extends ViewModel {
+    private LiveData<List<Route>> allRoutes;
+
+    public LiveData<List<Route>> getAllRoutes() {
+        return allRoutes;
+    }
+
 }
