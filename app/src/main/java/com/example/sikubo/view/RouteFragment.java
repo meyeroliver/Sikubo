2678b72726c60dd0cfb1594a2de48310fde6f7e3
omeyer <meyeroliver9401@gmail.com>
2019-02-26 12:05:55 +0200

    added onActivityCreate method to observe when the data is updated and update the UI

--- app/src/main/java/com/example/sikubo/view/RouteFragment.java
+++ app/src/main/java/com/example/sikubo/view/RouteFragment.java
@@ -5,14 +5,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.sikubo.model.Route;
+import com.example.sikubo.viewmodel.RouteViewModel;
+
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
 public class RouteFragment extends Fragment {
+
+    private RouteViewModel routeViewModel;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+       routeViewModel.getAllRoutes().observe(this, new Observer<List<Route>>() {
+           @Override
+           public void onChanged(List<Route> routes) {
+               /**
+                * TODO: update UI
+                */
+           }
+       });
+
     }
 }
