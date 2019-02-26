948c89fbce7e027a34a45afa3bcbdd190b26e7fe
omeyer <meyeroliver9401@gmail.com>
2019-02-26 17:04:05 +0200

    Calls the the route fragment

--- app/src/main/java/com/example/sikubo/RouteActivity.java
+++ app/src/main/java/com/example/sikubo/RouteActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.sikubo.view.RouteFragment;
+import com.example.sikubo.view.TransportFragment;
+
 public class RouteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container_route, new RouteFragment())
+                    .commit();
+        }
     }
 }
