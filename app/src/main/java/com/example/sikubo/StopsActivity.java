03b81defeee5d20db2445ebefd584cd601fe9098
omeyer <meyeroliver9401@gmail.com>
2019-02-28 01:09:23 +0200

    called in the proper fragmnent (Be careful when copying and pasting)

--- app/src/main/java/com/example/sikubo/StopsActivity.java
+++ app/src/main/java/com/example/sikubo/StopsActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.sikubo.view.RouteFragment;
+import com.example.sikubo.view.StopsFragment;
+
 public class StopsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stops);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container_stops, new StopsFragment())
+                    .commit();
+        }
     }
 }
