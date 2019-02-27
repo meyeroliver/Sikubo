3071e9bcc0cb5caef5274b7a9eca5abe547f8b37
omeyer <meyeroliver9401@gmail.com>
2019-02-27 09:04:25 +0200

    Provides a singlton for the goMetro instance

---
+++ app/src/main/java/com/example/sikubo/RetroClass.java
@@ -0,0 +1,23 @@
+package com.example.sikubo;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroClass {
+    public static final String BASE_URL = "http://proserver.gometro.co.za/api/v1";
+    private static Retrofit retrofit;
+    public static Retrofit getRetroInstance() {
+
+        if (retrofit == null) {
+          retrofit = new  Retrofit.Builder()
+                    .baseUrl("http://proserver.gometro.co.za/api/v1/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static GoMetroApi getGoMetroApi() {
+        return getRetroInstance().create(GoMetroApi.class);
+    }
+}
