fba2a67ce447850ffc047f58965c08e20e4ac5f6
omeyer <meyeroliver9401@gmail.com>
2019-02-27 17:28:22 +0200

    Made viewModal flexible to all transport routes

--- app/src/main/java/com/example/sikubo/viewmodel/RouteViewModel.java
+++ app/src/main/java/com/example/sikubo/viewmodel/RouteViewModel.java
@@ -3,35 +3,49 @@ package com.example.sikubo.viewmodel;
 import android.app.Application;
 
 import com.example.sikubo.Repository;
+import com.example.sikubo.model.GoldenArrow;
 import com.example.sikubo.model.Metrorail;
+import com.example.sikubo.model.MyCiti;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModel;
 
 public class RouteViewModel extends AndroidViewModel {
 
     private LiveData<List<Metrorail>> allRailRoutes;
+    private LiveData<List<MyCiti>> allTramRoutes;
+    private LiveData<List<GoldenArrow>> allBusRoutes;
     private Repository repository;
 
     public RouteViewModel(@NonNull Application application) {
         super(application);
-        repository = new Repository();
-    }
-
-    public void init(){
-        if (allRailRoutes != null) {
-            // ViewModel is created on a per-Fragment basis, so the userId
-            // doesn't change.
-            return;
-        }
-        allRailRoutes = repository.getAllRailRoutes();
+        this.repository = new Repository();
     }
 
     public LiveData<List<Metrorail>> getAllRailRoutes() {
-        return allRailRoutes;
+        if (this.allRailRoutes != null) {
+            return null;
+        }
+        this.allRailRoutes = this.repository.getAllRailRoutes();
+        return this.allRailRoutes;
+    }
+
+    public LiveData<List<MyCiti>> getAllTramRoutes() {
+        if (this.allTramRoutes != null) {
+            return  null;
+        }
+        this.allTramRoutes = this.repository.getAllTramRoutes();
+        return this.allTramRoutes;
+    }
+
+    public LiveData<List<GoldenArrow>> getAllBusRoutes() {
+        if (this.allBusRoutes != null){
+            return null;
+        }
+        this.allBusRoutes = this.repository.getAllBuslRoutes();
+        return this.allBusRoutes;
     }
 }
