448bac4cfc1334761f745aff478696382ff16d11
omeyer <meyeroliver9401@gmail.com>
2019-02-27 09:07:17 +0200

    This has been solid its my brain was not function last night

--- app/src/main/java/com/example/sikubo/viewmodel/RouteViewModel.java
+++ app/src/main/java/com/example/sikubo/viewmodel/RouteViewModel.java
@@ -1,17 +1,37 @@
 package com.example.sikubo.viewmodel;
 
-import com.example.sikubo.model.Route;
+import android.app.Application;
+
+import com.example.sikubo.Repository;
+import com.example.sikubo.model.Metrorail;
 
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-public class RouteViewModel extends ViewModel {
-    private LiveData<List<Route>> allRoutes;
+public class RouteViewModel extends AndroidViewModel {
 
-    public LiveData<List<Route>> getAllRoutes() {
-        return allRoutes;
+    private LiveData<List<Metrorail>> allRailRoutes;
+    private Repository repository;
+
+    public RouteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new Repository();
     }
 
+    public void init(){
+        if (allRailRoutes != null) {
+            // ViewModel is created on a per-Fragment basis, so the userId
+            // doesn't change.
+            return;
+        }
+        allRailRoutes = repository.getAllRailRoutes();
+    }
+
+    public LiveData<List<Metrorail>> getAllRailRoutes() {
+        return allRailRoutes;
+    }
 }
