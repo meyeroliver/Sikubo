fe4fcc92fa2f689a36a40ea51eeba2ede4df6ec9
omeyer <meyeroliver9401@gmail.com>
2019-02-27 02:30:28 +0200

    added functionality to getAllRailRoutes

--- app/src/main/java/com/example/sikubo/Repository.java
+++ app/src/main/java/com/example/sikubo/Repository.java
@@ -12,13 +12,16 @@ import java.util.List;
 import javax.inject.Singleton;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 @Singleton
 public class Repository {
 
     private GoMetroApi goMetroApi;
-    private LiveData<List<Metrorail>> allRailRoutes;
 
 
     public Repository(Application application) {
@@ -26,6 +29,26 @@ public class Repository {
     }
 
     public LiveData<List<Metrorail>> getAllRailRoutes() {
-        return allRailRoutes;
+        final MutableLiveData<List<Metrorail>> mutableLiveData = new MutableLiveData<>();
+        goMetroApi.getAllRailRoutes().enqueue(new Callback<List<Metrorail>>() {
+            @Override
+            public void onResponse(Call<List<Metrorail>> call, Response<List<Metrorail>> response) {
+                if (!response.isSuccessful()) {
+                   mutableLiveData.setValue(null);
+                    return;
+                }
+                mutableLiveData.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Metrorail>> call, Throwable t) {
+                /**
+                 * TODO: show errors
+                 */
+                mutableLiveData.setValue(null);
+            }
+        });
+
+        return mutableLiveData;
     }
 }
