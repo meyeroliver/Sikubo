1ff291d6ca07a64d72d155509c9e87aa6355f2a4
omeyer <meyeroliver9401@gmail.com>
2019-02-28 21:35:50 +0200

    this is a solid activity that calls the fragment

--- app/src/main/java/com/example/sikubo/LineUpdatesActivity.java
+++ app/src/main/java/com/example/sikubo/LineUpdatesActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.sikubo.view.LineUpdateFragment;
+
 public class LineUpdatesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_updates);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container_lineUpdates, new LineUpdateFragment())
+                    .commit();
+        }
     }
 }
