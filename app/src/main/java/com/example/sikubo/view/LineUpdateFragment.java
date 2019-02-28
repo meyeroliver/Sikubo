8c55f61cc986fb5dcf754349998c5451273f17e0
omeyer <meyeroliver9401@gmail.com>
2019-02-28 11:36:47 +0200

    next level up from base (now i need to implement the adaptor)

--- app/src/main/java/com/example/sikubo/view/LineUpdateFragment.java
+++ app/src/main/java/com/example/sikubo/view/LineUpdateFragment.java
@@ -4,29 +4,59 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sikubo.R;
+import com.example.sikubo.model.LineUpdates;
+import com.example.sikubo.viewmodel.LineUpdatesViewModel;
 
 
 public class LineUpdateFragment extends Fragment {
 
+    private LineUpdatesViewModel lineUpdatesViewModel;
+    private RecyclerView recyclerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_line_updates, container, false);
+
+        recyclerView = view.findViewById(R.id.lineUpdates_list_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        recyclerView.setHasFixedSize(true);
+
+        return view;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_line_update, container, false);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        lineUpdatesViewModel = ViewModelProviders.of(getActivity()).get(LineUpdatesViewModel.class);
+        /**
+         * TODO getStringExtra that carries the stop id
+         */
+        lineUpdatesViewModel.getRailLineUpdates("13:f12").observe(getActivity(), new Observer<LineUpdates>() {
+            @Override
+            public void onChanged(LineUpdates lineUpdates) {
+
+            }
+        });
+
     }
-
-
 }
