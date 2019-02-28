078f35353a854a896a5154b86d44c16e965418a9
omeyer <meyeroliver9401@gmail.com>
2019-02-28 08:33:39 +0200

    Base StopInfoActivity

---
+++ app/src/main/java/com/example/sikubo/StopInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.sikubo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StopInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_info);
+    }
+}
