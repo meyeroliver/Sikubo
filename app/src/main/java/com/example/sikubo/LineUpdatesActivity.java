5bbea8a05fa36e75a9d7b4bcc1456cf9379ce820
omeyer <meyeroliver9401@gmail.com>
2019-02-28 08:46:03 +0200

    base LineUpdates

--- app/src/main/java/com/example/sikubo/LineUpdatesActivity.java
+++ app/src/main/java/com/example/sikubo/LineUpdatesActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class StopInfoActivity extends AppCompatActivity {
+public class LineUpdatesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stop_info);
+        setContentView(R.layout.activity_line_updates);
     }
 }
