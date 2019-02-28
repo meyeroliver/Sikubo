4b3c5b77e1855be513b6bb5af91d24a052b09827
omeyer <meyeroliver9401@gmail.com>
2019-02-28 11:13:59 +0200

    base lineUpdateViewModel

---
+++ app/src/main/java/com/example/sikubo/viewmodel/LineUpdatesViewModel.java
@@ -0,0 +1,33 @@
+package com.example.sikubo.viewmodel;
+
+import android.app.Application;
+
+import com.example.sikubo.api.Repository;
+import com.example.sikubo.model.LineUpdates;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class LineUpdatesViewModel extends AndroidViewModel {
+
+    private LiveData<LineUpdates> railLineUpdates;
+    private Repository repository;
+
+    public LineUpdatesViewModel(@NonNull Application application) {
+        super(application);
+        this.repository = new Repository();
+    }
+
+    public LiveData<LineUpdates> getRailLineUpdats(String id) {
+
+        /**
+         * TODO: I believe the configuration error is right here
+         */
+        if (this.railLineUpdates != null) {
+            return null;
+        }
+        this.railLineUpdates = repository.getRailLineUpdats(id);
+        return this.railLineUpdates;
+    }
+}
